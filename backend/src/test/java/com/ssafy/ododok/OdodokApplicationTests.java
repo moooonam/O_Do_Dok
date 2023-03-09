e659301a8b2c996d8d71b24836a51ba9cb7566ad
audwl910 <audwl910@naver.com>
2023-03-09 11:23:07 +0900

    project init

---
+++ backend/src/test/java/com/ssafy/ododok/OdodokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ododok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdodokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
