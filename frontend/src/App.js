f68270b3d349af758e4be22e19f8df2a993c91d7
SSAFY <anska77@naver.com>
2023-03-13 17:25:39 +0900

    Frontend Init

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router} from "react-router-dom";
+import Heaader from './components/Header'
+function App() {
+  return (
+    <div>
+    <Heaader/>
+    <Router>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
