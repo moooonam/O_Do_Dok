864c6beda6c3a8e39e0315f2d009ab7c11a56975
SSAFY <anska77@naver.com>
2023-03-21 17:41:04 +0900

    feat: Update 여러 모달 추가

--- frontend/src/components/MyTeam/DodokBar.js
+++ frontend/src/components/MyTeam/DodokBar.js
@@ -1,62 +1,107 @@
-import React, {useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../../styles/MyTeamAfterDodok.module.scss";
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
 function DodokBar() {
-  useEffect(() => {
-    
-  }, [])
+  useEffect(() => {}, []);
+  const [pageReviewInfo, setpageReviewInfo] = useState({
+    page: "",
+    userName: "",
+    content: "",
+  });
   const bookPage = 300;
+  // 동기 비동기가 필요할거같아
   const pageReviews = [
     {
       id: 1,
       userName: "빵빵이",
-      userProfilImg: "https://item.kakaocdn.net/do/8d209a3c00ed5f23eeaa3758a1c7d59c7e6f47a71c79378b48860ead6a12bf11",
+      userProfilImg:
+      "https://item.kakaocdn.net/do/8d209a3c00ed5f23eeaa3758a1c7d59c7e6f47a71c79378b48860ead6a12bf11",
       page: 150,
+      content:
+      "어쩌구 저쩌구 어쩌구 저쩌구어쩌구 저쩌구 어쩌구 저쩌구 어쩌구 저쩌구 어쩌구 저쩌구 어쩌구 저쩌구 어쩌구 저쩌구 어쩌구 저쩌구 어쩌구 ",
     },
     {
       id: 2,
       userName: "채은이",
-      userProfilImg: "https://mblogthumb-phinf.pstatic.net/MjAxNzA2MTNfMSAg/MDAxNDk3MzI2NTk0Njcx.bs5-ntFT9Fv0PXd1yw_SSphKAYczGEUy7nn8eYqk1Hkg._6H5JZ-4ZVMaXDvjsWNOADSpwMbRNyNsaYwJcZI1ok4g.PNG.dksrnjscjf85/1.png?type=w800",
-      page: 145,
+      userProfilImg:
+      "https://mblogthumb-phinf.pstatic.net/MjAxNzA2MTNfMSAg/MDAxNDk3MzI2NTk0Njcx.bs5-ntFT9Fv0PXd1yw_SSphKAYczGEUy7nn8eYqk1Hkg._6H5JZ-4ZVMaXDvjsWNOADSpwMbRNyNsaYwJcZI1ok4g.PNG.dksrnjscjf85/1.png?type=w800",
+      page: 75,
+      content:
+      "어쩌구 저쩌구 어쩌구 저쩌구어쩌구 저쩌구 어쩌구 저쩌구 어쩌구 저쩌구 어쩌구 저쩌구 어쩌구 저쩌구 어쩌구 저쩌구 어쩌구 저쩌구 어쩌구 ",
     },
     {
       id: 3,
       userName: "빵빵이2",
-      userProfilImg: "https://image.aladin.co.kr/product/30818/49/cover500/s072831276_1.jpg",
-      page: 155,
+      userProfilImg:
+      "https://image.aladin.co.kr/product/30818/49/cover500/s072831276_1.jpg",
+      page: 225,
+      content:
+      "어쩌구 저쩌구 어쩌구 저쩌구어쩌구 저쩌구 어쩌구 저쩌구 어쩌구 저쩌구 어쩌구 저쩌구 어쩌구 저쩌구 어쩌구 저쩌구 어쩌구 저쩌구 어쩌구 ",
     },
   ];
-  // let bar = document.querySelector(`.${styles["lying-line"]}`)
-  // let barLength = bar.clientWidth
-  // console.log(typeof(barLength))
-  // const zIndex = 0
-  let barLength = (window.innerWidth)*0.84-150 +2
-  console.log(barLength)
-  let reviewBarWidth = window.innerWidth*0.025
+  const [pageReviewModal, setpageReviewModal] = React.useState(false);
+  const pageReviewModalOpen = () => {
+    setpageReviewModal(true);
+  };
+  const pageReviewModalClose = () => {
+    setpageReviewModal(false);
+  };
+  const clickPage = (pageReview) => setpageReviewInfo({
+    ...pageReviewInfo,
+    page: pageReview.page,
+    userName: pageReview.userName,
+    content: pageReview.content,
+  });
+  let barLength = window.innerWidth * 0.84 - 150 + 2;
+  let reviewBarWidth = window.innerWidth * 0.025;
   const renderPageReview = pageReviews.map((pageReview) => {
-    const position =  pageReview.page*barLength/bookPage
-
+    const position = (pageReview.page * barLength) / bookPage;
+    
     const style = {
-      marginLeft : `${position- reviewBarWidth}px`,
-      position: 'absolute',
+      marginLeft: `${position - reviewBarWidth}px`,
+      position: "absolute",
       zIndex: `${pageReview.page}`,
-      width: '5%',
-    }
+      width: "5%",
+    };
     return (
-        <div key={pageReview.id} style={style}>
-          <div className={styles["userImg-div"]}>
-            <img src={pageReview.userProfilImg} alt="프로필이미지" />
-          </div>
-          {/* <div>{pageReview.userName}</div> */}
-          <div className={styles["small-standing-line"]}></div>
+      <div key={pageReview.id} style={style}>
+        <div className={styles["userImg-div"]}>
+          <img
+            src={pageReview.userProfilImg}
+            alt="프로필이미지"
+            onClick={() => {
+              pageReviewModalOpen();
+              clickPage(pageReview);
+            }}
+          />
+          <Dialog
+            open={pageReviewModal} onClose={pageReviewModalClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">{pageReviewInfo.userName}님의 {pageReviewInfo.page}페이지 리뷰</DialogTitle>
+            <DialogContent>
+              <div>{pageReviewInfo.content}</div>
+              <div className={styles["wrap-modal-btn"]}>
+                <div className={styles["cancle-btn"]} onClick={pageReviewModalClose}>
+                  닫기
+                </div>
+              </div>
+            </DialogContent>
+          </Dialog>
         </div>
+        {/* <div>{pageReview.userName}</div> */}
+        <div className={styles["small-standing-line"]}></div>
+      </div>
     );
   });
   return (
     <div className={styles["wrap-bar"]}>
       <div className={styles["standing-line"]}></div>
       <div>{renderPageReview}</div>
-      <div className={styles["lying-line"]}>
-      </div>
+      <div className={styles["lying-line"]}></div>
       <div className={styles["standing-line"]}></div>
     </div>
   );
