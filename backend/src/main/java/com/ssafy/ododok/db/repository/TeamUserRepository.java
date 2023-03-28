d72e843ca8e5d38a39ea609800498594a8ac175b
김선규 <iwon4u@naver.com>
2023-03-28 11:56:51 +0900

    feat: 팀원 삭제 기능 수정

--- backend/src/main/java/com/ssafy/ododok/db/repository/TeamUserRepository.java
+++ backend/src/main/java/com/ssafy/ododok/db/repository/TeamUserRepository.java
@@ -4,6 +4,7 @@ import com.ssafy.ododok.db.model.Team;
 import com.ssafy.ododok.db.model.TeamUser;
 import com.ssafy.ododok.db.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,4 +18,7 @@ public interface TeamUserRepository extends JpaRepository<TeamUser,Long> {
     TeamUser findTeamUserByUser_UserId(Long userId);
 
     TeamUser findByUser_UserId(Long userId);
+
+    @Transactional
+    void deleteByUser_UserId(Long userId);
 }
