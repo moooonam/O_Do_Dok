6278fe6348dcffbbdd68d28321649f5cb7c65267
Chaeeun Jeong <jce0497@gmail.com>
2023-03-31 00:04:23 +0900

    feat: Add 유저 이미지 선택

--- frontend/src/pages/MyTeam/MyTeamMainPage.js
+++ frontend/src/pages/MyTeam/MyTeamMainPage.js
@@ -5,10 +5,11 @@ import mainstyles from "../../styles/MyTeamMain.module.scss";
 import { Api } from "../../Api";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import PanToolAltIcon from "@mui/icons-material/PanToolAlt";
 
 function MyTeamMainPage() {
   const movePage = useNavigate();
-  const myTeamId = useSelector((state) => state.user.myTeamId)
+  const myTeamId = useSelector((state) => state.user.myTeamId);
   const access_token = localStorage.getItem("access-token");
   const refresh_token = localStorage.getItem("refresh-token");
 
@@ -29,9 +30,11 @@ function MyTeamMainPage() {
     ongoingDodok: false,
   });
 
-  const [teamNotice, setTeamNotice] = useState({
-    notice: "",
-  })
+  const [teamArticle, setTeamArticle] = useState({
+    article1: "",
+    article2: "",
+    article3: "",
+  });
 
   useEffect(() => {
     Api.get("/user/myTeam", {
@@ -63,40 +66,38 @@ function MyTeamMainPage() {
       .catch((err) => {
         console.log(err);
       });
-      Api.get("/board/notice", {
-        headers: {
-          "refresh-token": `Bearer ${localStorage.getItem("refresh-token")}`,
-          "access-token": `Bearer ${localStorage.getItem("access-token")}`,
-        },
-      })
-        .then((res) => {
-          console.log("공지글 불러오기 완료----------");
-          const cnt = res.data.length
-          console.log(cnt)
-          console.log(res.data[cnt-1]);
-          setTeamNotice({
-            ...teamNotice,
-            notice: res.data[cnt-1]
-          })
-        })
-        .catch((err) => {
-          console.log(err);
+    Api.get("/board", {
+      headers: {
+        "refresh-token": `Bearer ${localStorage.getItem("refresh-token")}`,
+        "access-token": `Bearer ${localStorage.getItem("access-token")}`,
+      },
+    })
+      .then((res) => {
+        console.log("최신 게시글 불러오기 완료----------");
+        const cnt = res.data.length;
+        console.log(res);
+        setTeamArticle({
+          ...teamArticle,
+          article1: res.data[cnt - 1],
+          article2: res.data[cnt - 2],
+          article3: res.data[cnt - 3],
         });
-      // eslint-disable-next-line react-hooks/exhaustive-deps
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const goNotice = () => {
-    movePage(`/myteam/${myTeamId}/article/${teamNotice.notice.boardId}`)
-  }
+  // const goArticle = (id) => {
+  //   movePage(`/myteam/${myTeamId}/article/${id}`)
+  // }
 
   return (
     <div className={sidestyles["myteam-container"]}>
       <SideBar location={"main"} />
       <div className={sidestyles.others}>
         <div className={mainstyles["myteam-content-box"]}>
-          <h4 onClick={() => {goNotice()}}>
-            {/* 공지 | {teamNotice.notice.boardTitle} */}
-          </h4>
           <img
             src={teamDetail.teamImage}
             alt=""
@@ -149,46 +150,51 @@ function MyTeamMainPage() {
           </div>
           <div className={mainstyles["thirdBox"]}>
             <div className={mainstyles["myteam-newuserBox"]}>
-              <h3>신규 가입 인사</h3>
+              <h3>최신 게시글</h3>
               <div className={mainstyles["myteam-newuser"]}>
                 <div className={mainstyles["myteam-newuser-content"]}>
-                  <p>신규 가입자의 인사말이 들어갈 자리입니다</p>
-                  <p>신규 가입자의 인사말이 들어갈 자리입니다</p>
-                  <p>신규 가입자의 인사말이 들어갈 자리입니다</p>
+                  <p>{teamArticle.article1.boardTitle}</p>
+                  <p>{teamArticle.article2.boardTitle}</p>
+                  <p>{teamArticle.article3.boardTitle}</p>
                 </div>
                 <div className={mainstyles["myteam-newuser-name"]}>
-                  <p>사용자</p>
-                  <p>사용자</p>
-                  <p>사용자</p>
+                  {/* <p>{teamArticle.article1.user.userNickname}</p>
+                  <p>{teamArticle.article2.user.userNickname}</p>
+                  <p>{teamArticle.article2.user.userNickname}</p> */}
+                  {/* <p onClick={() => {goArticle(teamArticle.article1.boardId)}}>📃</p>
+                  <p onClick={() => {goArticle(teamArticle.article2.boardId)}}>📃</p>
+                  <p onClick={() => {goArticle(teamArticle.article3.boardId)}}>📃</p> */}
                 </div>
               </div>
             </div>
             <div className={mainstyles["myteam-dodokBox"]}>
               <h3>진행중인 도독</h3>
               <div>
-                { teamDetail.ongoingDodok === false
-                ? <h3 className={mainstyles["myteam-nododok"]}>진행중인 도독이 없습니다!</h3>
-                :
-                <div className={mainstyles["myteam-dodok"]}>
-                  <img
-                    src="https://image.aladin.co.kr/product/30929/51/cover500/k732831392_2.jpg"
-                    alt=""
-                    className={mainstyles["dodokBookImg"]}
-                  />
-                  <div className={mainstyles["myteam-dodok-title"]}>
-                    <p>도서명</p>
-                    <p>장르</p>
-                    <p>도독 시작</p>
-                    <p>도독 종료</p>
+                {teamDetail.ongoingDodok === false ? (
+                  <h3 className={mainstyles["myteam-nododok"]}>
+                    진행중인 도독이 없습니다!
+                  </h3>
+                ) : (
+                  <div className={mainstyles["myteam-dodok"]}>
+                    <img
+                      src="https://image.aladin.co.kr/product/30929/51/cover500/k732831392_2.jpg"
+                      alt=""
+                      className={mainstyles["dodokBookImg"]}
+                    />
+                    <div className={mainstyles["myteam-dodok-title"]}>
+                      <p>도서명</p>
+                      <p>장르</p>
+                      <p>도독 시작</p>
+                      <p>도독 종료</p>
+                    </div>
+                    <div className={mainstyles["myteam-dodok-content"]}>
+                      <p>책 이름</p>
+                      <p>장르 자리</p>
+                      <p>2023.03.09</p>
+                      <p>2023.03.15</p>
+                    </div>
                   </div>
-                  <div className={mainstyles["myteam-dodok-content"]}>
-                    <p>책 이름</p>
-                    <p>장르 자리</p>
-                    <p>2023.03.09</p>
-                    <p>2023.03.15</p>
-                  </div>
-                </div>
-                }
+                )}
               </div>
             </div>
           </div>
